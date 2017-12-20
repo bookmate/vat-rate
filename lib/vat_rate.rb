2f157dd6f1af40e301b7636ee14e924cd1a7c53c
korrs <slavakorr@gmail.com>
2017-12-20 12:32:51 +0300

    fetching vat from csv

--- lib/vat_rate.rb
+++ lib/vat_rate.rb
@@ -1,4 +1,24 @@
 require "vat_rate/version"
+require 'csv'
+
+DB_PATH = 'db/vat_rates.csv'.freeze
 
 module VatRate
+  def self.for(country_code)
+    country_code = country_code.to_s.upcase
+
+    DB.fetch[country_code]
+  end
+
+  class DB
+    def self.fetch
+      @data ||= begin
+        data = CSV.read(DB_PATH, headers: true)
+
+        data.each_with_object({}) do |line, hash|
+          hash[line['alpha2']] = line['vat'].to_f
+        end
+      end
+    end
+  end
 end
--- spec/vat_rate_spec.rb
+++ spec/vat_rate_spec.rb
@@ -1,4 +1,12 @@
 require "spec_helper"
 
 RSpec.describe VatRate do
+  describe '.for' do
+    subject { described_class.for(country_code) }
+    let(:country_code) { :ru }
+
+    specify do
+      expect(subject).to eq 18.0
+    end
+  end
 end
