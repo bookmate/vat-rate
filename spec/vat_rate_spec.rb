91984fd5d02eb22049497fea50ef6e73d2927b7f
Nikolay Markov <mainameiz@gmail.com>
2019-01-22 17:33:51 +0300

    Fix date of new RU VAT rate

--- spec/vat_rate_spec.rb
+++ spec/vat_rate_spec.rb
@@ -16,8 +16,8 @@ RSpec.describe VatRate do
       subject { described_class.for(country_code, date) }
 
       it 'returns vat rate actual on that date' do
-        expect(described_class.for(country_code, Date.new(2017, 12, 31))).to eq(18.0)
-        expect(described_class.for(country_code, Date.new(2018, 1, 1))).to eq(20.0)
+        expect(described_class.for(country_code, Date.new(2018, 12, 31))).to eq(18.0)
+        expect(described_class.for(country_code, Date.new(2019, 1, 1))).to eq(20.0)
       end
     end
   end
